2fb42bed4ab86a2a4f1ea065d1046b4796a8c35e
Vishalakshi Pathak <vishapathak21@gmail.com>
2025-09-10 22:23:10 +0530

    git project

---
+++ src/Component/About.jsx
@@ -0,0 +1,5 @@
+ export default function About(){
+    return(
+        <h1 className="item- center">Welcome to about page </h1>
+    )
+}
---
+++ src/Component/Home.jsx
@@ -0,0 +1,5 @@
+ export default function Home(){
+    return(
+        <h1 className="item center">Welcome to home page</h1>
+    )
+}
